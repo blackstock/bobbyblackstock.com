4b8c238a894b0ac2bb36a5c68261de247d2f57db
Bobby Blackstock <bobbyblackstock@gmail.com>
2015-10-18 13:22:31 -0600

    move header to react component

--- javascripts/components/header.js
+++ javascripts/components/header.js
@@ -1,32 +1,13 @@
-var navBar = React.createClass({
+var header = React.createClass({
   render: function() {
     return (
-      <div className="container-fluid">
-        <div className="navbar-header">
-          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-                  data-target="#bs-example-navbar-collapse-1">
-            <span className="sr-only">Toggle navigation</span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-            <span className="icon-bar"></span>
-          </button>
-          <a className="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
-        </div>
-        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-          <ul className="nav navbar-nav navbar-right">
-            <li>
-              <a className="page-scroll" href="#about">About</a>
-            </li>
-            <li>
-              <a className="page-scroll" href="#services">Services</a>
-            </li>
-            <li>
-              <a className="page-scroll" href="#portfolio">Portfolio</a>
-            </li>
-            <li>
-              <a className="page-scroll" href="#contact">Contact</a>
-            </li>
-          </ul>
+      <div className="header-content">
+        <div className="header-content-inner">
+          <h1>Your Favorite Source of Free Bootstrap Themes</h1>
+          <hr/>
+          <p>Start Bootstrap can help you build better websites using the Bootstrap CSS framework! Just download your
+            template and start going, no strings attached!</p>
+          <a href="#about" className="btn btn-primary btn-xl page-scroll">Find Out More</a>
         </div>
       </div>
     )
@@ -34,6 +15,6 @@ var navBar = React.createClass({
 });
 
 ReactDOM.render(
-  React.createElement(navBar),
-  document.getElementById('mainNav')
+  React.createElement(header),
+  document.getElementById('header')
 );
---
+++ javascripts/components/navbar.js
@@ -0,0 +1,42 @@
+var navBar = React.createClass({
+  render: function() {
+    return (
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">Bobby Blackstock</a>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a className="page-scroll" href="#about">About</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#services">Skills</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#portfolio">Resume</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+});
+
+ReactDOM.render(
+  React.createElement(navBar),
+  document.getElementById('mainNav')
+);
