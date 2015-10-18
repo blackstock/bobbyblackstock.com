e8ca543929b9e9559e956a24d6a991bdbcbf0b00
Bobby Blackstock <bobbyblackstock@gmail.com>
2015-10-18 12:54:08 -0600

    trying to precompile react components

--- javascripts/components/header.js
+++ javascripts/components/header.js
@@ -1,32 +1,39 @@
-/** @jsx React.DOM */
 React.render(
-  <div className="container-fluid">
-    <div className="navbar-header">
-      <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-              data-target="#bs-example-navbar-collapse-1">
-        <span className="sr-only">Toggle navigation</span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-        <span className="icon-bar"></span>
-      </button>
-      <a className="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
-    </div>
-    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-      <ul className="nav navbar-nav navbar-right">
-        <li>
-          <a className="page-scroll" href="#about">About</a>
-        </li>
-        <li>
-          <a className="page-scroll" href="#services">Services</a>
-        </li>
-        <li>
-          <a className="page-scroll" href="#portfolio">Portfolio</a>
-        </li>
-        <li>
-          <a className="page-scroll" href="#contact">Contact</a>
-        </li>
-      </ul>
-    </div>
-  </div>,
+  <navBar/>,
   document.getElementById("mainNav")
 );
+
+var navBar = React.createClass({
+  render: function() {
+    return (
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                  data-target="#bs-example-navbar-collapse-1">
+            <span className="sr-only">Toggle navigation</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand page-scroll" href="#page-top">Start Bootstrap</a>
+        </div>
+        <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a className="page-scroll" href="#about">About</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#services">Services</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#portfolio">Portfolio</a>
+            </li>
+            <li>
+              <a className="page-scroll" href="#contact">Contact</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+});
