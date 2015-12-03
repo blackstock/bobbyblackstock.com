ea08e1036b98f0d140b5e62e640d974d989dd4cc
Bobby Blackstock <bobbyblackstock@gmail.com>
2015-12-03 14:13:38 -0700

    tweaking contact section

--- javascripts/components/contact.js
+++ javascripts/components/contact.js
@@ -4,27 +4,27 @@ var contact = React.createClass({
       <div>
         <div className="container">
           <div className="row">
-            <div className="col-lg-12 col-lg-offset-2 text-center">
+            <div className="col-lg-12 text-center">
               <h2 className="section-heading">Contact Me</h2>
               <hr className="primary"/>
               <p>Want to talk tech? Looking for some consulting or some part-time software
                 development? Please don't hesitate to contact me. Thanks!</p>
             </div>
-            <div className="col-lg-4 col-lg-offset-2 text-center">
+            <div className="col-lg-4 text-center">
               <a href="https://www.facebook.com/bobby.blackstock" target="_blank">
                 <i className="fa fa-facebook-official fa-3x wow bounceIn"></i>
 
                 <p>Facebook</p>
               </a>
             </div>
-            <div className="col-lg-4 col-lg-offset-2 text-center">
+            <div className="col-lg-4 text-center">
               <a href="https://twitter.com/bobbyblackstock" target="_blank">
                 <i className="fa fa-twitter-square fa-3x wow bounceIn"></i>
 
                 <p>Twitter</p>
               </a>
             </div>
-            <div className="col-lg-4 col-lg-offset-2 text-center">
+            <div className="col-lg-4 text-center">
               <a href="https://www.linkedin.com/in/bobbyblackstock" target="_blank">
                 <i className="fa fa-linkedin-square fa-3x wow bounceIn"></i>
 
