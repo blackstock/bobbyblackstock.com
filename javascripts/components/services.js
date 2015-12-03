6b583ed84634e92d61b3dbc41cd37fd26194d976
Bobby Blackstock <bobbyblackstock@gmail.com>
2015-12-03 13:49:52 -0700

    test

--- javascripts/components/services.js
+++ javascripts/components/services.js
@@ -17,19 +17,13 @@ var services = React.createClass({
                 <i className="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
 
                 <h3>Ruby on Rails</h3>
-
-                <p className="text-muted">Rails is a model–view–controller (MVC) framework,
-                  providing default structures for a database, a web service, and web pages.</p>
               </div>
             </div>
             <div className="col-lg-3 col-md-6 text-center">
               <div className="service-box">
-                <i className="fa fa-4x fa-paper-plane wow bounceIn text-primary" data-wow-delay=".1s"></i>
+                <i className="fa fa-4x fa-html5 wow bounceIn text-primary" data-wow-delay=".1s"></i>
 
                 <h3>HTML/CSS</h3>
-
-                <p className="text-muted"> HyperText Markup Language and CSS for presentation,
-                  style the appearance of web pages.</p>
               </div>
             </div>
             <div className="col-lg-3 col-md-6 text-center">
@@ -37,17 +31,52 @@ var services = React.createClass({
                 <i className="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
 
                 <h3>JavaScript</h3>
-
-                <p className="text-muted">A high-level, dynamic, untyped, and interpreted programming language.</p>
               </div>
             </div>
             <div className="col-lg-3 col-md-6 text-center">
               <div className="service-box">
-                <i className="fa fa-4x fa-heart wow bounceIn text-primary" data-wow-delay=".3s"></i>
+                <i className="fa fa-4x fa-server wow bounceIn text-primary" data-wow-delay=".3s"></i>
 
-                <h3>Accounting/Finance</h3>
+                <h3>SQL Databases</h3>
+              </div>
+            </div>
+          </div>
 
-                <p className="text-muted"> Certified Public Accountant in the state of Colorado.</p>
+          <div className="row">
+            <div className="col-lg-12 text-center">
+              <h2 className="section-heading">Skills</h2>
+              <hr className="primary"/>
+            </div>
+          </div>
+        </div>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-diamond wow bounceIn text-primary"></i>
+
+                <h3>Ruby on Rails</h3>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-html5 wow bounceIn text-primary" data-wow-delay=".1s"></i>
+
+                <h3>HTML/CSS</h3>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-newspaper-o wow bounceIn text-primary" data-wow-delay=".2s"></i>
+
+                <h3>JavaScript</h3>
+              </div>
+            </div>
+            <div className="col-lg-3 col-md-6 text-center">
+              <div className="service-box">
+                <i className="fa fa-4x fa-server wow bounceIn text-primary" data-wow-delay=".3s"></i>
+
+                <h3>SQL Databases</h3>
               </div>
             </div>
           </div>
