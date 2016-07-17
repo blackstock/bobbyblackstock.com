9d739715054624304d90860d3142530db8171e72
Bobby Blackstock <bobbyblackstock@gmail.com>
2016-07-16 18:32:20 -0600

    mess with coloring

--- javascripts/components/header.js
+++ javascripts/components/header.js
@@ -4,7 +4,7 @@ var header = React.createClass({
       <div className="header-content">
         <div className="header-content-inner">
           <h1>Bobby Blackstock</h1>
-          <p></p>
+          <hr className="light"/>
           <p>Hello and welcome! My name is Bobby and I'm a software developer, Certified Public Accountant (CPA) and
             a self-proclaimed entrepreneur.</p>
           <a href="#about" className="btn btn-primary btn-xl page-scroll">About Me</a>
